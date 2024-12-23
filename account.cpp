e55e83806a0522827c467a852808235d92bedafb
RomeoCoding <165141163+RomeoCoding@users.noreply.github.com>
2024-12-23 18:11:34 +0200

    Update account.cpp

--- account.cpp
+++ account.cpp
@@ -1,13 +1,15 @@
 #include "error_handler.hpp"
 #include <stdexcept>
-
+#include "account.hpp"
 Account::Account(const std::string& id, const std::string& password, double initial_balance)
     : id(id), password(password), balance(initial_balance) {}
 
 std::string Account::get_id() const {
     return id;
 }
-
+std::string Account::get_password() const {
+    return password;
+}
 double Account::view_balance() const {
     return balance;
 }
@@ -17,4 +19,12 @@ bool Account::authenticate(const std::string& input_password) const {
         return false;
     }
     return true;
+}
+void Account::deposit(double amount) {
+    balance += amount;
+}
+
+void Account::withdraw(double amount) {
+    
+        balance -= amount;
 }
