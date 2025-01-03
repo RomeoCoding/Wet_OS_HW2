a014d5bb9cd08c8a302fe227e9f3643714b20263
RomeoCoding <165141163+RomeoCoding@users.noreply.github.com>
2025-01-03 12:27:18 +0200

    Update account.hpp

--- account.hpp
+++ account.hpp
@@ -2,15 +2,18 @@
 #define ACCOUNT_HPP
 
 #include <string>
+#include <pthread.h>
 
 class Account {
 private:
     std::string id;
     std::string password;
     double balance;
+    pthread_rwlock_t balance_rwlock; //Read-write lock for protecting the balance
 
 public:
     Account(const std::string& id, const std::string& password, double initial_balance);
+    ~Account(); 
 
     std::string get_id() const;
     std::string get_password() const;
@@ -20,7 +23,6 @@ public:
     void withdraw(double amount);
     void deposit(double amount);
     void print_account_details() const;
-
 };
 
 #endif // ACCOUNT_HPP
